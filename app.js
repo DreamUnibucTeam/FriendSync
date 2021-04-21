cc037e04b2303f944da907c85733c74547e1d149
Nicu-Ducal <nicu.ducal01@gmail.com>
2021-04-21 19:49:54 +0300

    Adding boilerplate for routing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT ?? 8000;
+
+/* API Routes */
+app.use("/api/auth", require(path.join(__dirname, "router", "auth.router.js")));
 
 app.get("/", (req, res) => {
   res.send("REST API Server for FriendSync application");
