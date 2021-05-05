808495886f7c91c8a4fccb65c8b5b745de2060d7
Nicu Ducal <59502994+Nicu-Ducal@users.noreply.github.com>
2021-05-05 16:25:25 +0300

    adding firebase functions to client

---
+++ client/src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default firebaseConfig = {
+  apiKey: "AIzaSyC9DcwR-ZdP2artarN4PQWmEdiyZysuABg",
+  authDomain: "friendsync-5fc52.firebaseapp.com",
+  projectId: "friendsync-5fc52",
+  storageBucket: "friendsync-5fc52.appspot.com",
+  messagingSenderId: "1053703274792",
+  appId: "1:1053703274792:web:b48592a92af2133bb8901d",
+};
