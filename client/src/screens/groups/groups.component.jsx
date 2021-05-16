0d4602e15e3dcfce3422367bb6ca7d2a0ca6f8e5
DanielAvram1 <daniel.avram@my.fmi.unibuc.ro>
2021-05-16 16:59:58 +0300

    started connecting groups with server

--- client/src/screens/groups/groups.component.jsx
+++ client/src/screens/groups/groups.component.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState, useContext } from "react";
 
 import { Text, View, TextInput, ScrollView, TouchableOpacity } from "react-native";
 import { Icon } from "react-native-elements";
+import {useHttp} from "../../hooks/http.hook"
+import {UserContext} from '../../context/UserContext'
+import {auth} from '../../firebase/firebase'
 
 import groupsStyles from "./groups.styles";
 import groupsData from "./groups.data";
@@ -9,7 +12,37 @@ import groupsData from "./groups.data";
 import Group from "../../components/group/group.component";
 
 const Groups = ({navigation}) => {
+  const { request, loading, REST_API_LINK, error } = useHttp()
   const [groupList, setGroupList] = useState(groupsData)
+  const [user, setUser] = useContext(UserContext)
+
+  const getGroups = useCallback(async () => {
+    try {
+      const token = await auth.currentUser.getIdToken()
+      const data = await request(`${REST_API_LINK}/api/users/belongsTo/groups/${user.uid}`, 'GET', null, {
+        Authorization: `Bearer ${token}`
+      })
+      setGroupList(data)
+      console.log(data)
+    } catch (error) {
+      console.log("Error @GroupsComponent/getGroups: ", error.message)
+    }
+  }, [request])
+
+  useEffect(() => {
+    getGroups()
+  }, [getGroups])
+
+  // groupId, name
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    )
+  }
+
   return (
     <ScrollView style={groupsStyles.container}>
       <View
@@ -33,23 +66,23 @@ const Groups = ({navigation}) => {
 
       <View style={groupsStyles.groupList}>
         {groupList.map(
-          ({ id, groupName, groupPhotoUrl, lastMessage }) => (
+          ({ groupId, name }) => (
             <TouchableOpacity
               onPress={() =>navigation.navigate("ChatStack", {
                 screen: 'Chat',
                 params: {
-                  id:id, 
-                  groupName:groupName, 
-                  groupPhotoUrl:groupPhotoUrl
+                  id:groupId, 
+                  groupName:name, 
+                  groupPhotoUrl:user.profilePhotoUrl,
                 }
               })}
-              key={id}
+              key={groupId}
             >
               <Group
                 
-                groupName={groupName}
-                groupPhotoUrl={groupPhotoUrl}
-                lastMessage={lastMessage}
+                groupName={name}
+                groupPhotoUrl={user.profilePhotoUrl}
+                lastMessage={"last message"}
               />
             </TouchableOpacity>
           )
