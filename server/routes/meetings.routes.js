0470c00127cfe614ad538e4b5ecf7c5325ab3795
Nicu Ducal <59502994+Nicu-Ducal@users.noreply.github.com>
2021-05-21 18:55:39 +0300

    Added API endpoints for meetings and activities
    Co-authored-by: Laura Tender <lauratender@users.noreply.github.com>

---
+++ server/routes/meetings.routes.js
@@ -0,0 +1,30 @@
+const Router = require("express");
+const {
+  checkAuthenticationToken,
+} = require("../middleware/auth.middleware.js");
+const MeetingController = require("../controllers/meeting.controller");
+
+const router = Router();
+const controller = MeetingController.getInstance();
+
+/* Meeting routes */
+router.get("/meeting/:id", controller.getMeeting);
+
+router.get("/group/:id", controller.getAllMeetings);
+
+router.post("/meeting", controller.createMeeting);
+
+router.put("/meeting/schedule/:id", controller.getBestInterval);
+
+router.put("/meeting/location/:id", controller.setLocation);
+
+/* Activity routers */
+router.get("/activity/:id", controller.getAllActivities);
+
+router.post("/activity", controller.createActivity);
+
+router.put("/activity/addvote/:id", controller.addVote);
+
+router.put("/activity/removevote/:id", controller.removeVote);
+
+module.exports = router;
